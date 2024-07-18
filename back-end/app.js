f57034c1dac7be37175f4722f90d5103ab3ecb6e
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-07-17 21:11:24 -0400

    add songs controller index route

--- back-end/app.js
+++ back-end/app.js
@@ -2,8 +2,13 @@
 const cors = require("cors");
 const express = require("express");
 
+
 // Configuration
 const app = express();
+
+// Controllers
+const songsController = require("./controllers/songController.js")
+app.use("/songs", songsController);
 
 // Middleware
 app.use(cors());
