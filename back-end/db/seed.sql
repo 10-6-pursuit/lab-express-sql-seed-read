715a713954adb7c7c3f6187835a004d3e445d7ab
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-07-17 21:41:46 -0400

    add more songs

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -3,4 +3,6 @@
 INSERT INTO
  songs ( name, artist, album, time, is_favorite )
 VALUES
- ('Cherry Blossom', 'Empire Of The Sun', 'Cheery Blossom', '3:28', true);
+ ('Cherry Blossom', 'Empire Of The Sun', 'Cheery Blossom', '3:28', true),
+ ('My Kink Is Karma', 'Chappell Roan', 'The Rise and Fall of a Midwest Princess', '3:43', true),
+ ('Expresso', 'Sabrina Carpenter', 'Expresso', '2:55', true);
