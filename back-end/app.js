c442aabd1b0d570ac5b154fcb953dece653f7b8b
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-07-20 09:31:30 -0400

    db connected to server and retrieving data from tables

--- back-end/app.js
+++ back-end/app.js
@@ -1,14 +1,18 @@
 const cors = require("cors");
 const express = require("express");
-
+const songsController = require("./controllers/songsController.js")
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
     res.status(200).send("<h1>Songs Server</h1>");
 });
+
+app.use("/songs", songsController);
+
 
 app.get("*", (req, res) => {
     res.status(404).send("<h1>404 Page Not Found</h1>");
