3aa90939efbd499e1ed60e60f1081f4586a9969b
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-07-19 20:26:39 -0400

    add show route to songController.js

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/song");
+const { getAllSongs, getSong } = require("../queries/song");
 
 // Index Route
 songs.get("/", async (req, res) => {
@@ -11,5 +11,16 @@ songs.get("/", async (req, res) => {
         res.status(500).json({ error: "server error"});
     }
 })
+
+// Show Route
+songs.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const song = await getSong(id);
+    if (song) {
+        res.status(200).json(song);
+    } else {
+        res.status(404).json({ error: "not found" });
+    }
+});
 
 module.exports = songs;
