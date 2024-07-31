f1bafd1e0514902793d06dfed79dd5730b019533
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-07-30 21:38:28 -0400

    frontend setup complete

--- tuner-front-end/src/App.jsx
+++ tuner-front-end/src/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 export default function App() {
-  return <></>;
+  return (
+    <div className="app-container">
+      <h1>Tuner App</h1>
+    </div>
+  );
 }
