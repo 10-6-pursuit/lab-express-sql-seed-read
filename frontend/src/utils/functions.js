ba7f13165a84b08e46b2a8d1f8bd81e71383c8e0
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-08-04 20:42:40 -0400

    function.js updated for playlist

--- frontend/src/utils/functions.js
+++ frontend/src/utils/functions.js
@@ -151,3 +151,18 @@ function toSecs(str) {
   const [min, secs] = str.split(":").map(Number);
   return min * 60 + secs;
 }
+
+// Get all playlists
+export async function getPlaylists() {}
+
+// Get playlist
+export async function getPlaylist(id) {}
+
+// Create playlist
+export async function createPlaylist() {}
+
+// Add song to playlist
+export async function addSongToPlaylist(id, songId) {}
+
+// Remove song from playlist
+export async function removeSongFromPlaylist(id, songId) {}
