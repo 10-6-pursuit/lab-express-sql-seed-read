b9e9d4135a1d943e3e8a0e05f1fb27995b8362c9
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-08-17 16:14:55 -0400

    added playlist componenet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home.jsx";
 import Nav from "./components/Nav/Nav.jsx";
 import NotFound from "./components/NotFound/NotFound.jsx";
+import Playlists from "./components/Playlists/Playlists.jsx";
+import Playlist from "./components/Playlist/Playlist.jsx";
 import Song from "./components/Song/Song.jsx";
 import SongForm from "./components/SongForm/SongForm.jsx";
 import Songs from "./components/Songs/Songs.jsx";
 
 import "./App.css";
-import Playlist from "./components/Playlist/Playlist.jsx";
 
 export default function App() {
   return (
@@ -24,7 +25,8 @@ export default function App() {
           <Route path="/songs/:id" element={<Song />} />
           <Route path="/songs/new" element={<SongForm />} />
           <Route path="/songs/:id/edit" element={<SongForm />} />
-          <Route path="/playlists" element={<Playlist />} />
+          <Route path="/playlists" element={<Playlists />} />
+          <Route path="/playlists/:id" element={<Playlist />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
