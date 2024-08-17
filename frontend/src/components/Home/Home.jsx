f95db84bd9a2dd43e4f8d0ab7d9f3eb0787b1c1c
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-08-17 16:00:57 -0400

    stash

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,23 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Home.css";
-import { getPlaylists } from "../../utils/playlistFetch";
 
 export default function Home() {
-  const [playlists, setPlaylists] = useState([]);
-
-  useEffect(() => {
-    async function fetchPlaylists() {
-      try {
-        const data = await getPlaylists();
-        setPlaylists(data);
-      } catch (error) {
-        console.error("Error fetching playlists:", error);
-      }
-    }
-
-    fetchPlaylists();
-  }, []);
-
   return (
     <div className="home-container">
       <h1>Welcome to the Tuner app</h1>
@@ -32,21 +16,6 @@ export default function Home() {
         favorite songs in one spot. It allows you to edit and delete any song
         you put in. Click on the Songs tab to view your songs.
       </p>
-      <div className="playlists-container">
-        <h2>Your Playlists</h2>
-        {playlists.length > 0 ? (
-          <ul>
-            {playlists.map((playlist) => (
-              <li key={playlist.id}>
-                <h3>{playlist.name}</h3>
-                <p>{playlist.description}</p>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No playlists found. Start creating some!</p>
-        )}
-      </div>
     </div>
   );
 }
